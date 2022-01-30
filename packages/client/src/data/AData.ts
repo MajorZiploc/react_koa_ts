ffb943133012013ab372b6f6dc19e8364aeef971
Manyu Lakhotia <lakemonyew@gmail.com>
2022-01-30 13:47:59 -0600

    Rm path join,update react-scripts to 5.0.0

--- packages/client/src/data/AData.ts
+++ packages/client/src/data/AData.ts
@@ -1,12 +1,11 @@
 import { AppSettings, Movie } from '../models';
 import IData from './IData';
-import { join } from 'path';
 
 export default abstract class AData implements IData {
   private _config: Promise<AppSettings>;
 
   constructor() {
-    const url = join(__dirname, 'appsettings.json');
+    const url = `${process.env.PUBLIC_URL}appsettings.json`;
     this._config = fetch(url, {
       mode: 'cors',
       headers: {
