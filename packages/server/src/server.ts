c6816ad607017a1de7450ee97f1f885380a23530
Manyu Lakhotia <lakemonyew@gmail.com>
2022-01-30 14:54:38 -0600

    Fixes port for server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 import databaseConnection from './database/database.connection';
 
-const PORT: number = Number(process.env.PORT) || 3000;
-databaseConnection.then(() => app.listen(PORT)).catch(console.error);
+const SERVER_PORT: number = Number(process.env.SERVER_PORT) || 6000;
+databaseConnection.then(() => app.listen(SERVER_PORT)).catch(console.error);
